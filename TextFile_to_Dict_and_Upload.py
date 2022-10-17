e844189ece8b2e9d69cfab3c3f3867c946845dd6
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-17 13:44:29 -0700

    Update TextFile_to_Dict_and_Upload.py
    Made correction to syntax of link to website in POST request; missing '/'

--- TextFile_to_Dict_and_Upload.py
+++ TextFile_to_Dict_and_Upload.py
@@ -20,4 +20,4 @@ for entry in os.listdir(directory):
     # prints error message that did not process entry since it wasn't a file
     print("Error: Did not process; entry is not a file: " + str(entry))
 # makes a POST request to post the dictionary to company website
-requests.post(http://<corpweb-external-IP>/feedback, data=feedback_dict)
+requests.post(http://<corpweb-external-IP>/feedback/, data=feedback_dict)
