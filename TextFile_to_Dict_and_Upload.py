6e95a399c1779bdcc9a5781ffb36849a37328ef2
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-17 15:44:03 -0700

    Made edits to comments
    Made edits to comments to better describe the function of parts of the script

--- TextFile_to_Dict_and_Upload.py
+++ TextFile_to_Dict_and_Upload.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 
+# imports the necessary modules
 import os
 import requests
 import json
 
+# initializes variables and dictionaries
 directory = '/data/feedback/'
 feedback_dict = {}
 
-# return a list of all files and directories in specified directory
+# returns a list of all files and directories in specified directory
 for entry in os.listdir(directory):
   path = str(directory) + str(entry)
-  #check if entry is a file
+  # checks if entry is a file
   if os.path.isfile(path):
     with open(path) as input:
+      # splits the text file by line into a list and then adds the list values as values in dictionary
       input_line = input.read().split('\n')
       feedback_dict['title'] = input_line[0].strip()
       feedback_dict['name'] = input_line[1].strip()
       feedback_dict['date'] = input_line[2].strip()
       feedback_dict['feedback'] = input_line[3].strip()
-      # makes a POST request to post the dictionary to company website
+      # makes a POST request to post the dictionary to company website and then prints the response status code
       response = requests.post("http://<corpweb-external-IP>/feedback/", json=feedback_dict)
       print(response.status_code)
   else:
