4d7dd7a74a650b5a5fc8e29da709b651e97b6a56
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-17 14:17:27 -0700

    Moved POST request line into for loop
    Moved POST request line into for loop because the script was only making a POST request for the first text file in the directory.

--- TextFile_to_Dict_and_Upload.py
+++ TextFile_to_Dict_and_Upload.py
@@ -16,8 +16,9 @@ for entry in os.listdir(directory):
       feedback_dict['name'] = input_line[1].strip()
       feedback_dict['date'] = input_line[2].strip()
       feedback_dict['feedback'] = input_line[3].strip()
+      # makes a POST request to post the dictionary to company website
+      requests.post(http://<corpweb-external-IP>/feedback/, data=feedback_dict)
   else:
     # prints error message that did not process entry since it wasn't a file
     print("Error: Did not process; entry is not a file: " + str(entry))
-# makes a POST request to post the dictionary to company website
-requests.post(http://<corpweb-external-IP>/feedback/, data=feedback_dict)
+
