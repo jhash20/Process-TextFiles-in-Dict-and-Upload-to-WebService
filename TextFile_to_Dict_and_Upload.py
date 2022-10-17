973a07adbea27910b92227e78e9e92cb712dc647
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-17 14:30:11 -0700

    Added response.status_code
    Added print(response.status_code) to ensure each request was successful

--- TextFile_to_Dict_and_Upload.py
+++ TextFile_to_Dict_and_Upload.py
@@ -19,6 +19,7 @@ for entry in os.listdir(directory):
       feedback_dict['feedback'] = input_line[3].strip()
       # makes a POST request to post the dictionary to company website
       requests.post(http://<corpweb-external-IP>/feedback/, data=feedback_dict)
+      print(response.status_code)
   else:
     # prints error message that did not process entry since it wasn't a file
     print("Error: Did not process; entry is not a file: " + str(entry))
