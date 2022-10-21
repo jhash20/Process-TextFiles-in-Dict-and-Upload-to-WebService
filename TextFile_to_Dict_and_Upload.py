ddd3461697b2b7d47f968847a05adb7c13c0ab5d
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-20 19:15:05 -0700

    Standardized formatting per PEP
    Added DocString
    
    Removed unnecessary comments
    Made comments more useful

--- TextFile_to_Dict_and_Upload.py
+++ TextFile_to_Dict_and_Upload.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 
-# imports the necessary modules
+"""Script that processes text files and uploads to a web service endpoing
+by converting text files information (feedback) into a Python dictionary 
+and then uploading to a running Django Web service by making a POST request.
+Also, prints a response status code.
+"""
+
 import os
 import requests
 import json
 
-# initializes variables and dictionaries
+
 directory = '/data/feedback/'
 feedback_dict = {}
 
-# returns a list of all files and directories in specified directory
+# iterates through entries in directory
 for entry in os.listdir(directory):
   path = str(directory) + str(entry)
-  # checks if entry is a file
+  # ensures that entry being iterated is a file
   if os.path.isfile(path):
     with open(path) as input:
-      # splits the text file by line into a list and then adds the list values as values in dictionary
+      # splits by line and then adds the lines as values in dictionary
       input_line = input.read().split('\n')
       feedback_dict['title'] = input_line[0].strip()
       feedback_dict['name'] = input_line[1].strip()
       feedback_dict['date'] = input_line[2].strip()
       feedback_dict['feedback'] = input_line[3].strip()
-      # makes a POST request to post the dictionary to company website and then prints the response status code
+
       response = requests.post("http://<corpweb-external-IP>/feedback/", json=feedback_dict)
       print(response.status_code)
   else:
-    # prints error message that did not process entry since it wasn't a file
     print("Error: Did not process; entry is not a file: " + str(entry))
 
